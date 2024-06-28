cf5b55118139e6313e356bda3f3fbefd50379eae
Lucas Gean dos Santos <lucasgeansantos@gmail.com>
2024-06-28 09:45:32 -0300

    Update dialog_create_meeting.tsx

--- components/admin/meetings/dialog_create_meeting/dialog_create_meeting.tsx
+++ components/admin/meetings/dialog_create_meeting/dialog_create_meeting.tsx
@@ -31,7 +31,7 @@ function DialogRegisterMeeting({ setMeetings }: DialogRegisterMeetingProps) {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="bg-black/50 data-[state=open]:animate-overlayShow fixed inset-0 z-10" />
-        <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-background p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none z-10">
+        <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-background p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none z-10">
           <Dialog.Title className="text-[20px] font-medium">
             Cadastre a reunião
           </Dialog.Title>
